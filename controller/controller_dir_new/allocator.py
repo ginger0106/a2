0357d781e0e8b1d2aac4ba869378223789cbebea
ginger0106 <840909love@163.com>
2019-08-28 10:21:26 +0800

    add bw

--- controller/controller_dir_new/allocator.py
+++ controller/controller_dir_new/allocator.py
@@ -355,7 +355,7 @@ class allocator():
                                     resultt[k,q,f'{k}_dcp_{i}'] = {'url': url_list, 'model_ver': i, 'data_ver': j,
                                                               'batch': h, 'prob': y[s, k, i, j, q, h]}
                                     # re[k,q] = resultt
-                                    re.update({(k,q):{f'{k}_dcp_{i}':resultt[q,f'{k}_dcp_{i}']}})
+                                    re.update({(k,q):{f'{k}_dcp_{i}':resultt[k,q,f'{k}_dcp_{i}']}})
                                     # for key,items in resultt.items():
                                     #
                                     #     if (k,q) in [key[:2]]:
